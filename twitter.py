65793eb79be86c6689b037041369490967dafbf9
Mārcis Gailītis <57437437+MarcisGailitis@users.noreply.github.com>
2020-04-28 12:42:12 +0300

    Update chek_for_dir and save_file functions
    os.path.join added to make script work on Win and Linux workstations

--- twitter.py
+++ twitter.py
@@ -5,26 +5,16 @@ from datetime import date
 from dotenv import load_dotenv
 
 
-def chek_for_dir():
-    today_yyyy=date.today().year
-    today_mm=date.today().month
-    yyyy_mm=str(today_yyyy)+'-'+str(today_mm)
-    this_month_dir='data/'+str(yyyy_mm)
-    if not os.path.isdir(this_month_dir):
-        os.mkdir(this_month_dir)
-        print('Creating directory', this_month_dir)
-    else:
-        print('directory already in place', this_month_dir)
-
-
 def get_url():
     load_dotenv()
     consumer_key = os.getenv('TW_consumer_key')
     consumer_secret = os.getenv('TW_consumer_secret')
     token_key = os.getenv('TW_token_key')
     token_secret = os.getenv('TW_token_secret')
+
     auth = OAuth1(consumer_key, consumer_secret, token_key, token_secret)
     url = 'https://api.twitter.com/1.1/trends/place.json'
+    
     #854823 for Riga, 23424874 forLatvia
     woeid = {'854823':'riga','23424874':'latvia'}
     places = {'id':'854823'}
@@ -34,23 +24,30 @@ def get_url():
     # print headers
     print('Remaining', headers['x-rate-limit-remaining'],'from',headers['x-rate-limit-limit'])
 
-def save_file():
-    today = date.today()
-    today_yyyy = date.today().year
-    today_mm = date.today().month
-    yyyy_mm = str(today_yyyy)+'-'+str(today_mm)
 
-    with open('data/'+str(yyyy_mm)+'/'+woeid['854823']+'_'+str(today)+'.json','w') as h_out:
-        h_out.write(r.text)
-        h_out.close()
-    print(h_out,'created')
+def chek_for_dir(this_month_dir):
+    if not os.path.isdir(this_month_dir):
+        os.mkdir(this_month_dir)
+        print('Creating directory:', this_month_dir)
+    else:
+        print('Directory already created:', this_month_dir)
 
+
+def save_file(response):
+    yyyy_mm = str(date.today().year)+'-'+str(date.today().month)
+    this_month_dir = os.path.join(os.getcwd(), str(yyyy_mm))
+    woeid = {'854823': 'Riga'}
+    filename = woeid['854823']+'_'+str(date.today())+'.json'
+
+    chek_for_dir(this_month_dir)
+    with open(os.path.join(this_month_dir, filename), 'w') as h_out:
+        h_out.write(response.text)
+    print(h_out, 'created')
 
 
 def main():
-    get_url()
-    chek_for_dir()
-    save_file()
+    # get_url()
+    response = 'asd'
+    save_file(response)
 
 main()
-
